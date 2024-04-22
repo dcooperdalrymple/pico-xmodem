0ba7100992d540d66df9d43f1c137c1bda0726ac
dcooperdalrymple <dcooperdalrymple@gmail.com>
2024-04-22 12:36:20 -0500

    Improved test implementation and added data relay loop.

--- example/pico-xmodem-example.cpp
+++ example/pico-xmodem-example.cpp
@@ -1,30 +1,43 @@
 #include <stdio.h>
+#include <tusb.h>
 #include "pico/stdlib.h"
 #include "xmodem.hpp"
 
 #define MAXSIZE 32768
-static uint8_t buffer[MAXSIZE];
+#define MODE XMode::Original
 
-static XMODEM xmodem(XMode::CRC);
+static uint8_t buffer[MAXSIZE];
+static XMODEM xmodem(MODE);
 
 int main() {
     // NOTE: Not necessary to call `stdio_init_all()`
-    printf("Pico C/C++ XMODEM Library Example\r\n");
 
     xmodem.set_log_level(XLogLevel::Debug);
 
     uint8_t i;
     size_t size;
     while (true) {
-        size = xmodem.receive(buffer, MAXSIZE);
-        if (size > 0) {
-            printf("Received %d bytes\r\n", size);
-        }
-        if (size > 8) {
-            for (i = 0; i < 8; i++) {
-                printf("%02X ", buffer[i]);
+        while (!tud_cdc_connected()) sleep_ms(100);
+        printf("\r\nPico C/C++ XMODEM Library Example\r\n");
+
+        while (true) {
+            printf("Ready to receive data. Begin sending file...\r\n");
+            size = xmodem.receive(buffer, MAXSIZE);
+            if (size > 0) {
+                printf("Received %d bytes\r\n", size);
+                if (size > 8) {
+                    for (i = 0; i < 8; i++) {
+                        printf("%02X ", buffer[i]);
+                    }
+                    printf("\r\n");
+                }
+                printf("Sending back data. Begin receive...\r\n");
+                if (xmodem.send(buffer, size)) {
+                    printf("Successfully delivered!\r\n");
+                } else {
+                    printf("Failed to deliver.\r\n");
+                }
             }
-            printf("\r\n");
         }
     }
 
